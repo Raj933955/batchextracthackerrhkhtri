e198d26be280e563d629e671cad5d21d403833ea
Raj933955 <162988577+Raj933955@users.noreply.github.com>
2024-05-06 19:59:04 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 27952989
+api_hash = "74f04808a359e9a516e955ec243613ca"
+bot_token = "6799548363:AAHdF2AKWsgM23HKlNAaTmWoth21tVzigrA"
+auth_users = [5128979564]
+sudo_user = 5128979564
+log_channel = -1002047255106
+txt_channel = -1002047255106
